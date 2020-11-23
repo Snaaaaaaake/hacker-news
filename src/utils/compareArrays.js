b033999a0ab59308efd78c74cb2bedc1f8e5e165
Snaaaaaaake <kissmyshinyass@mail.ru>
2020-11-23 22:43:37 +0300

    Updates

--- src/utils/compareArrays.js
+++
@@ -1,5 +0,0 @@
-export default function compareArrays(array1, array2) {
-  if (array1.length !== array2.length) return false;
-  if (array1.length === 0) return true;
-  return array1.every((item, index) => item === array2[index]);
-}
